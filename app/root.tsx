777ac65668b15f8088dc8b4aa9f5b813c7ea2b7b
Abdul Zahoor <its4zahoor@gmail.com>
2023-11-05 02:15:07 +0500

    Synchronizing URLs to Form State
    Signed-off-by: Abdul Zahoor <its4zahoor@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import {
   useLoaderData,
   useNavigation,
 } from "@remix-run/react";
-
+import { useEffect } from "react";
 import { createEmptyContact, getContacts } from "./data";
 
 import appStylesHref from "./app.css";
@@ -25,7 +25,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return json({ contacts });
+  return json({ contacts, q });
 };
 
 export const action = async () => {
@@ -34,8 +34,15 @@ export const action = async () => {
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <html lang="en">
@@ -53,6 +60,7 @@ export default function App() {
               <input
                 id="q"
                 aria-label="Search contacts"
+                defaultValue={q || ""}
                 placeholder="Search"
                 type="search"
                 name="q"
